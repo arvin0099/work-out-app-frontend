74481a95d63f1e8d6f07bd420ee70cab95d5ee86
linguosity <brandon@linguosity.ai>
2024-04-08 20:42:53 -0700

    install Tailwind CSS

--- workout-app/src/App.js
+++ workout-app/src/App.js
@@ -2,6 +2,7 @@ import {Routes, Route, useNavigate} from 'react-router-dom';
 import {useState} from 'react';
 import Login from './components/Login';
 import Signup from './components/Signup';
+import Home from './components/Home';
 import './App.css'
 
 import logo from './logo.svg';
@@ -64,6 +65,7 @@ function App() {
     <div className="App">
       
       <Routes>
+        <Route path='/home' element={<Home /> }/>
         <Route path='/login' element={<Login handleLogin={handleLogin}></Login>} />
         <Route path='/signup' element={ <Signup handleSignup={handleSignUp}></Signup>} />
       </Routes>
---
+++ workout-app/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+    <h1 class="text-3l font-bold underline">
+    Hello world!!
+  </h1>
+    </div>
+  )
+}
+
+export default Home;

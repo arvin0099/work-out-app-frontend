24f0f43f1da55a542eb1a8308e09daf73c85664e
linguosity <brandon@linguosity.ai>
2024-04-09 21:52:16 -0700

    add modals for login and signup

--- workout-app/src/components/Navbar.jsx
+++ workout-app/src/components/Navbar.jsx
@@ -5,11 +5,46 @@ import Signup from './Signup';
 function Navbar(props) {
   return (
     <>
-   
 
-        <Login props={props}/>
-        <Signup props={props}/>
-      
+    <div className="navbar bg-base-100">
+        <div className="flex-none">
+            <button className="btn btn-square btn-ghost">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+            </button>
+        </div>
+        <div className="flex-1">
+            <a className="btn btn-ghost text-xl">daisyUI</a>
+        </div>
+        <div className="flex-none">
+            <button className="btn btn-square btn-ghost">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
+            </button>
+        </div>
+        {/*Modal button and modal box for login*/}
+        {/* Open the modal using document.getElementById('ID').showModal() method */}
+        <button className="btn" onClick={()=>document.getElementById('my_modal_1').showModal()}>open modal</button>
+        <dialog id="my_modal_1" className="modal">
+            <div className="modal-box">
+                <Login props={props}/>
+            </div>
+            <form method="dialog" className="modal-backdrop">
+                <button>close</button>
+            </form>
+        </dialog>
+
+
+        {/*Modal button and modal box for signup*/}
+        <button className="btn" onClick={()=>document.getElementById('my_modal_2').showModal()}>open modal</button>
+        <dialog id="my_modal_2" className="modal">
+            <div className="modal-box">
+                 <Signup props={props}/>
+            </div>
+            <form method="dialog" className="modal-backdrop">
+                <button>close</button>
+            </form>
+        </dialog>
+    </div>
+
     </>
   )
 }
